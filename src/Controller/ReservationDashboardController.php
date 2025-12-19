cc10f43ebdc8933ea20f92390d44cafe904dcc6b
Maxime <>
2025-12-19 09:37:31 +0100

    Feat : creation de l'env pour create reservation

--- src/Controller/ReservationDashboardController.php
+++ src/Controller/ReservationDashboardController.php
@@ -28,4 +28,22 @@ final class ReservationDashboardController extends AbstractController
             'reservations' => $reservations,
         ]);
     }
+    #[Route('/reservation/create', name: 'app_reservation_create')]
+    public function create(ApiClient $api, RequestStack $stack): Response
+    {
+        try {
+            $data = $api->get('/api/ministages');
+            $reservations = array_values($data) ?? [];
+        } catch (\Throwable $e) {
+            $reservations = [];
+            echo 'error: ' . $e->getMessage();
+            if ($stack->getSession()) {
+                $this->addFlash('error', 'Impossible de récupérer les reservations (API) : '.$e->getMessage());
+            }
+        }
+
+        return $this->render('reservation_dashboard/create.html.twig', [
+            'reservations' => $reservations,
+        ]);
+    }
 }
